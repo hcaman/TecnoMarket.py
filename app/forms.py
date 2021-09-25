92065e8d81f3313219e968a428565bdae43592db
hugo cm <hugocmdesign@gmail.com>
2021-09-26 00:38:45 +0200

    use crispy req

--- app/forms.py
+++ app/forms.py
@@ -3,6 +3,9 @@ from .models import Contacto
 
 
 class ContactoForm(forms.ModelForm):
+
+    # nombre = forms.CharField(widget=forms.TextInput(attrs={"class": "form-control"}))
+
     class Meta:
         model = Contacto
         # fields = ["nombre", "correo", "tipo_consulta", "mensaje", "avisos"]

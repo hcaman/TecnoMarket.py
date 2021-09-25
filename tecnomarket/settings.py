222930fd0ce34ff236232971ca7ab45e49b8aa98
hugo cm <hugocmdesign@gmail.com>
2021-09-25 18:37:54 +0200

    add the format price number

--- tecnomarket/settings.py
+++ tecnomarket/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -40,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'app.apps.AppConfig',
     'colorfield',
+    'django.contrib.humanize',
 ]
 
 X_FRAME_OPTIONS = 'SAMEORIGIN'
@@ -130,7 +132,6 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-import os 
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")

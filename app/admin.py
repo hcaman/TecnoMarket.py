6b46530a6ab2a418aa0f56036c20eabd65ea8baf
hugo cm <hugocmdesign@gmail.com>
2021-09-25 15:10:54 +0200

    change the ModelAdmin inside products

--- app/admin.py
+++ app/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 from .models import Marca, Producto
 # Register your models here.
+class ProductoAdmin(admin.ModelAdmin):
+  list_display = ["nombre", "precio", "nuevo", "marca"]
+  list_editable = ["precio"]
+  search_fields = ["nombre"]
+  list_filter = ["nombre", "nuevo"]
+
 admin.site.register(Marca)
-admin.site.register(Producto)
+admin.site.register(Producto, ProductoAdmin)

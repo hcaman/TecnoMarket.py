aa886dbf739ca9ae976934b84589e30345c47294
hugo cm <hugocmdesign@gmail.com>
2021-09-25 19:13:30 +0200

    Add a model for contacts view

--- app/models.py
+++ app/models.py
@@ -1,20 +1,42 @@
 from django.db import models
 
 # Create your models here.
-class Marca(models.Model):
-  nombre = models.CharField(max_length=50)
 
-  def __str__(self):
-    return self.nombre
+
+class Marca(models.Model):
+    nombre = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nombre
+
 
 class Producto(models.Model):
-  nombre = models.CharField(max_length=50)
-  precio = models.IntegerField()
-  descripcion = models.TextField()
-  nuevo = models.BooleanField()
-  marca = models.ForeignKey(Marca, on_delete=models.PROTECT)
-  fecha_fabricacion = models.DateField()
-  image = models.ImageField(upload_to="productos", null=True)
+    nombre = models.CharField(max_length=50)
+    precio = models.IntegerField()
+    descripcion = models.TextField()
+    nuevo = models.BooleanField()
+    marca = models.ForeignKey(Marca, on_delete=models.PROTECT)
+    fecha_fabricacion = models.DateField()
+    image = models.ImageField(upload_to="productos", null=True)
 
-  def __str__(self):
-    return self.nombre
+    def __str__(self):
+        return self.nombre
+
+
+opciones_consultas = [
+    [0, "consulta"],
+    [1, "reclamo"],
+    [2, "sugerencia"],
+    [3, "felicitaciones"],
+]
+
+
+class Contacto(models.Model):
+    nombre = models.CharField(max_length=50)
+    correo = models.EmailField()
+    tipo_consulta = models.IntegerField(choices=opciones_consultas)
+    mensaje = models.TextField()
+    avisos = models.BooleanField()
+
+    def __str__(self):
+        return self.nombre

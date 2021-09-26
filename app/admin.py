6a8fa4118e64681bffb1a4f6a6403f944e22b41d
hugo cm <hugocmdesign@gmail.com>
2021-09-26 22:16:55 +0200

    use the same validations for the admin

--- app/admin.py
+++ app/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import Marca, Producto, Contacto
+from .forms import ProductoForm
 # Register your models here.
 
 
@@ -9,6 +10,7 @@ class ProductoAdmin(admin.ModelAdmin):
     search_fields = ["nombre"]
     list_filter = ["nombre", "nuevo"]
     list_per_page = 20
+    form = ProductoForm
 
 
 admin.site.register(Marca)
